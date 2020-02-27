572849cc66e5b6a8f7ecb2c7236b2f43d51e1436
GloryNwa <glorynwa@gmail.com>
2020-02-27 11:55:16 +0000

    Updates

---
+++ precache-manifest.e4ba43a9df5b76d7fbc48d710a804037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba044052abdde75b3e811afb4f1c5fc0",
+    "url": "/ContactManager-App/index.html"
+  },
+  {
+    "revision": "31e091e8e31cc1f1381d",
+    "url": "/ContactManager-App/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0edd93a064ff9733cdd4",
+    "url": "/ContactManager-App/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "31e091e8e31cc1f1381d",
+    "url": "/ContactManager-App/static/js/2.1c90f26d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/ContactManager-App/static/js/2.1c90f26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edd93a064ff9733cdd4",
+    "url": "/ContactManager-App/static/js/main.f9adfb3e.chunk.js"
+  },
+  {
+    "revision": "cebb6285f8f0d9f70423",
+    "url": "/ContactManager-App/static/js/runtime-main.7a3fa150.js"
+  }
+]);
